02ea6d33cef4470158aeaeee042f445755649ece
Neil Boyd <NeilBoyd@gmail.com>
2026-06-22 16:19:46 +0200

    undo the initial thing

--- src/components/WeekPlanner.jsx
+++ src/components/WeekPlanner.jsx
@@ -11,8 +11,6 @@ const MEMBER_COLORS = {
   Kevin: "#e8c247",
 };
 
-const MEMBER_INITIALS = { Papa: "N", Mama: "L", Inga: "I", Kevin: "K" };
-
 function computeWarnings(days, weekPlan, recipes) {
   const warningFilters = PICKER_FILTERS.filter((f) => f.threshold != null);
   const counts = {};
@@ -115,7 +113,7 @@ export default function WeekPlanner({ days, family, weekPlan, weekOffset, onWeek
           <div className="corner-cell"></div>
           {family.map((m) => (
             <div key={m} className="member-header" style={{ borderBottom: `3px solid ${MEMBER_COLORS[m]}` }}>
-              {MEMBER_INITIALS[m] || m[0]}
+              {m}
             </div>
           ))}
         </div>
